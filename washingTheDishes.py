ceb90200602360008318d0ef60466b78e89aa680
pranavfiji <69599542+pranavfiji@users.noreply.github.com>
2021-08-05 20:04:19 -0400

    Add files via upload

---
+++ washingTheDishes.py
@@ -0,0 +1,22 @@
+
+from scipy import misc
+import matplotlib.pyplot as plt
+plt.close('all')
+class WashingTheDishes:
+    def _init_(self,washingTheDishes):
+        self.washingTheDishes=washingTheDishes
+
+    def applySoapToSponge():
+        im=misc.imread('soap.png')
+        plt.imshow(im)
+        print("Get the soap and put it on the sponge")
+
+    def putDishesInDishWasher():
+        print("Dish Washer has been put in")
+
+def main():
+    print("Soap has been applied")
+
+
+    
+
